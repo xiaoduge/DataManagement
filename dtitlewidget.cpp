33f73dcee9951d265cbef5cd0d7195826c8b25ba
chengjun.du <654653458@qq.com>
2018-02-27 14:53:31 +0800

    自定义标题栏

---
+++ dtitlewidget.cpp
@@ -0,0 +1,6 @@
+#include "dtitlewidget.h"
+
+DTitleWidget::DTitleWidget(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ dtitlewidget.h
@@ -0,0 +1,16 @@
+#ifndef DTITLEWIDGET_H
+#define DTITLEWIDGET_H
+
+
+class DTitleWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DTitleWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // DTITLEWIDGET_H
